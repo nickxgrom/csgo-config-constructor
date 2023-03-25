43d7b4ccbe6f6d9abd1a7ea0f0311526dfa3fda2
Nick Gromyko <nxgr.dev@gmail.com>
2023-03-25 23:54:48 +0600

    add(dependencies): i18n

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import mdiVue from "mdi-vue/v3";
 import * as mdijs from '@mdi/js'
 
 import router from './router'
-
 const pinia = createPinia()
 const app = createApp(App)
+
+import {createI18n} from "vue-i18n";
+import ru from "@/locales/ru.js";
+import en from "@/locales/en.js";
 
 app.use(mdiVue, {
     icons: mdijs
@@ -17,5 +20,15 @@ app.use(mdiVue, {
 app.component('$router', router)
 
 app.use(router).use(pinia)
+
+const i18n = createI18n({
+    legacy: false,
+    messages: {
+        'en-US': en,
+        'ru-RU': ru
+    }
+})
+
+app.use(i18n)
 
 app.mount('#app')
