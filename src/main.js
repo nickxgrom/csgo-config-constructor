8a30572002a9c873644344bbcdde061033fc7583
Nick Gromyko <nxgr.dev@gmail.com>
2023-03-26 00:03:23 +0600

    update(main.js): const belong imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-const app = createApp(App)
 import App from './App.vue'
 import './styles/style.scss'
 
@@ -13,6 +12,7 @@ import { createI18n } from 'vue-i18n'
 import ru from '@/locales/ru.js'
 import en from '@/locales/en.js'
 
+const app = createApp(App)
 const pinia = createPinia()
 const i18n = createI18n({
     legacy: false,
