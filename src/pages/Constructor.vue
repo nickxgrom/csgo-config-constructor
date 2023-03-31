18fd2422d0c2acbbe8e57ccea5872fe12a0d4b3a
Nick Gromyko <nxgr.dev@gmail.com>
2023-03-31 20:58:17 +0600

    update(src/pages/Constructor.vue): CSelect test

--- src/pages/Constructor.vue
+++ src/pages/Constructor.vue
@@ -2,12 +2,33 @@
 	<div class="page-title">
 		<h1>{{ $t('pages.constructor.title') }}</h1>
 	</div>
+	<div class="container">
+		<div class="categories">
+			<c-select
+				:options="options"
+			/>
+		</div>
+		<div>wip</div>
+	</div>
 </template>
 
-<script>
-export default {
-	name: "Constructor.vue"
-}
+<script setup>
+import CSelect from "@/components/shared/CSelect.vue";
+import {useI18n} from "vue-i18n";
+
+const i18n = useI18n()
+
+const options = [
+	{
+		text: i18n.t('pages.constructor.categorySelect.sound'),
+		value: 'sound'
+	},
+	{
+		text: i18n.t('pages.constructor.categorySelect.voice'),
+		value: 'voice'
+	},
+]
+
 </script>
 
 <style scoped>
