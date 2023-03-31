380433dfd58e4f5d50fcea67f4e4b31d9d5c8456
Nick Gromyko <nxgr.dev@gmail.com>
2023-03-31 20:56:45 +0600

    add(src/components/shared/CSelect.vue): initial component structure

---
+++ src/components/shared/CSelect.vue
@@ -0,0 +1,33 @@
+<template>
+	<select name="c-select" id="c-select">
+		<option
+			v-for="(option, index) in options"
+			:key="index"
+			:selected="index === selectedId"
+			:value="option.value"
+		>
+			{{ option.text }}
+		</option>
+	</select>
+</template>
+
+<script setup>
+defineProps({
+	options: {
+		type: Array,
+		required: true
+	},
+	selectedId: {
+		type: Number,
+		default: 0
+	},
+	disabled: {
+		type: Boolean,
+		default: false
+	},
+	size: {
+		type: Number,
+		default: 0
+	}
+})
+</script>
